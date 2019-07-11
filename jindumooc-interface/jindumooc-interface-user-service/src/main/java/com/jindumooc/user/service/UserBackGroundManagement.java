112d31f83bd78510d014ea023d0020b769364f8e
shangpenghui123456789 <2448306109@qq.com>
2019-07-11 16:01:31 +0800

    Trival Fixed

--- jindumooc-interface/jindumooc-interface-user-service/src/main/java/com/jindumooc/user/service/UserBackGroundManagement.java
+++ jindumooc-interface/jindumooc-interface-user-service/src/main/java/com/jindumooc/user/service/UserBackGroundManagement.java
@@ -2,7 +2,6 @@ package com.jindumooc.user.service;
 
 import com.jindumooc.pojo.User;
 import java.util.*;
-import com.github.pagehelper.PageInfo;
 import com.jindumooc.vojo.BackGroundIndexUser;
 
 public interface UserBackGroundManagement {
