099a51cf34d14c4275d865ab8a48e5dc753fe691
mahongsheng <863253972@qq.com>
2019-09-20 23:00:40 +0800

    配置课程模块

--- jindumooc-course-service/src/main/java/com/jindumooc/JindumoocCourseServiceApplication.java
+++ jindumooc-course-service/src/main/java/com/jindumooc/JindumoocCourseServiceApplication.java
@@ -1,9 +1,13 @@
 package com.jindumooc;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo//开启Dubbo
 @SpringBootApplication
+@MapperScan(basePackages = "com.jindumooc.dao")
 public class JindumoocCourseServiceApplication {
 
     public static void main(String[] args) {
